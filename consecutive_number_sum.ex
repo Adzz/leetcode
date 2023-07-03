3c94c61602da2ee6b9665a3cf84ca55e540f9f0c
Adam Lancaster <lancaster28@hotmail.com>
2023-07-03 15:11:08 -0500

    Implements consecutive number sum without using as much mems
    ...But does so in a way that is still too slow.

--- consecutive_number_sum.ex
+++ consecutive_number_sum.ex
@@ -1,4 +1,8 @@
 defmodule Solution do
+  @moduledoc """
+  https://leetcode.com/problems/consecutive-numbers-sum
+
+  """
   @spec consecutive_numbers_sum(n :: integer) :: integer
   def consecutive_numbers_sum(1), do: 1
   def consecutive_numbers_sum(0), do: 0
@@ -6,32 +10,26 @@ defmodule Solution do
   def consecutive_numbers_sum(n) do
     # So for large lists this will be a lot of memory. Instead
     # is there a number formula? Spoiler. Yes there is a maths trick.
-    sum(Enum.to_list(1..(n - 1)), n, 1)
+    sum(1, n, 1)
   end
 
-  defp sum([], _target, total), do: total
+  defp sum(current, target, total) when current == target - 1, do: total
 
-  defp sum(range, target, total) do
-    case consume_until_bust(range, 0, target) do
-      :bust -> sum(tl(range), target, total)
-      :hit -> sum(tl(range), target, total + 1)
+  defp sum(starting_value, target, total) do
+    case consume_until_bust(starting_value, 0, target) do
+      :bust -> sum(starting_value + 1, target, total)
+      :hit -> sum(starting_value + 1, target, total + 1)
     end
   end
 
-  defp consume_until_bust([], running_total, running_total), do: :hit
-
-  defp consume_until_bust([], _, _total) do
-    :bust
-  end
-
-  defp consume_until_bust([head | rest], running_total, target) do
+  defp consume_until_bust(head, running_total, target) do
     current = head + running_total
 
     if current > target do
       :bust
     else
       if current < target do
-        consume_until_bust(rest, current, target)
+        consume_until_bust(head + 1, current, target)
       else
         :hit
       end
@@ -41,17 +39,25 @@ end
 
 defmodule Main do
   def run() do
-    # 5
-    # |> Solution.consecutive_numbers_sum()
-    # |> IO.inspect(limit: :infinity, label: "1")
+    5
+    |> Solution.consecutive_numbers_sum()
+    |> IO.inspect(limit: :infinity, label: "1")
 
-    # 9
-    # |> Solution.consecutive_numbers_sum()
-    # |> IO.inspect(limit: :infinity, label: "2")
+    9
+    |> Solution.consecutive_numbers_sum()
+    |> IO.inspect(limit: :infinity, label: "2")
+
+    15
+    |> Solution.consecutive_numbers_sum()
+    |> IO.inspect(limit: :infinity, label: "3")
+
+    3
+    |> Solution.consecutive_numbers_sum()
+    |> IO.inspect(limit: :infinity, label: "3")
 
     5_809_342
     |> Solution.consecutive_numbers_sum()
-    |> IO.inspect(limit: :infinity, label: "3")
+    |> IO.inspect(limit: :infinity, label: "4")
   end
 end
 
