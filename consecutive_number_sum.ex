3b1c826cbe59b14ae55c2120c184e22ce5152916
Adam Lancaster <lancaster28@hotmail.com>
2023-07-05 17:19:45 -0500

    Adds a version that works up to a given number size.
    Ideas for further improvement are to use binary search when looking for
    a chain length if the search space is large.

--- consecutive_number_sum.ex
+++ consecutive_number_sum.ex
@@ -3,35 +3,102 @@ defmodule Solution do
   https://leetcode.com/problems/consecutive-numbers-sum
 
   """
-  @spec consecutive_numbers_sum(n :: integer) :: integer
+  @doc """
+  """
+  def consecutive_numbers_sum(2), do: 1
   def consecutive_numbers_sum(1), do: 1
   def consecutive_numbers_sum(0), do: 0
 
   def consecutive_numbers_sum(n) do
-    # So for large lists this will be a lot of memory. Instead
-    # is there a number formula? Spoiler. Yes there is a maths trick.
-    sum(1, n, 1)
-  end
+    longest_chain_length = longest_chain(1, div(n, 2) + 1, n)
+    longest_chain_length |> IO.inspect(limit: :infinity, label: "ll")
+    # If the largest number of consecutive integers is 2 then there will not be a 3 chain
+    # of ints so stop immediately.
+    if longest_chain_length <= 2 do
+      longest_chain_length
+    else
+      {total, _} =
+        (longest_chain_length - 1)..2
+        |> Enum.reduce({2, 1}, fn max_chain_length, {total, min_starting_value} ->
+          max_starting_value = div(n, max_chain_length)
 
-  defp sum(current, target, total) when current == target - 1, do: total
+          if min_starting_value > max_starting_value do
+            {total, min_starting_value}
+          else
+            search_chain_size(
+              min_starting_value,
+              max_chain_length,
+              min_starting_value,
+              max_starting_value,
+              n,
+              total
+            )
+          end
+        end)
 
-  defp sum(starting_value, target, total) do
-    case consume_until_bust(starting_value, 0, target) do
-      :bust -> sum(starting_value + 1, target, total)
-      :hit -> sum(starting_value + 1, target, total + 1)
+      total
     end
   end
 
-  defp consume_until_bust(head, running_total, target) do
+  defp longest_chain(starting_value, max_starting_value, target) do
+    case sum_with_count(starting_value, max_starting_value, 0, 0, target) do
+      :bust -> longest_chain(starting_value + 1, max_starting_value, target)
+      int_count -> int_count
+    end
+  end
+
+  # Probably this can be improved... probably by binary searching the starting value.
+  defp sum_with_count(head, max_starting_value, running_total, int_count, target) do
     current = head + running_total
 
     if current > target do
       :bust
     else
       if current < target do
-        consume_until_bust(head + 1, current, target)
+        next = head + 1
+
+        if next <= max_starting_value do
+          sum_with_count(next, max_starting_value, current, int_count + 1, target)
+        else
+          :bust
+        end
       else
-        :hit
+        int_count + 1
+      end
+    end
+  end
+
+  defp search_chain_size(starting_value, _, min_starting_value, max_starting_value, _, total)
+       when starting_value > max_starting_value do
+    {total, min_starting_value}
+  end
+
+  defp search_chain_size(
+         starting_value,
+         chain_length,
+         min_starting_value,
+         max_starting_value,
+         target,
+         total
+       ) do
+    chain_total = chain_length / 2 * (starting_value + (starting_value + (chain_length - 1)))
+
+    if chain_total > target do
+      # Is there a way to bust out faster for a given window size?
+      {total, starting_value + 1}
+    else
+      if chain_total == target do
+        {total + 1, starting_value + 1}
+      else
+        # chain_total < target
+        search_chain_size(
+          starting_value + 1,
+          chain_length,
+          min_starting_value,
+          max_starting_value,
+          target,
+          total
+        )
       end
     end
   end
@@ -41,23 +108,39 @@ defmodule Main do
   def run() do
     5
     |> Solution.consecutive_numbers_sum()
-    |> IO.inspect(limit: :infinity, label: "1")
+    |> IO.inspect(limit: :infinity, label: "1 Answer (should be 2):")
 
     9
     |> Solution.consecutive_numbers_sum()
-    |> IO.inspect(limit: :infinity, label: "2")
+    |> IO.inspect(limit: :infinity, label: "2 Answer (3):")
 
     15
     |> Solution.consecutive_numbers_sum()
-    |> IO.inspect(limit: :infinity, label: "3")
+    |> IO.inspect(limit: :infinity, label: "3 Answer (4):")
 
     3
     |> Solution.consecutive_numbers_sum()
-    |> IO.inspect(limit: :infinity, label: "3")
+    |> IO.inspect(limit: :infinity, label: "4 Answer (2):")
+
+    10
+    |> Solution.consecutive_numbers_sum()
+    |> IO.inspect(limit: :infinity, label: "5 Answer (2):")
+
+    45
+    |> Solution.consecutive_numbers_sum()
+    |> IO.inspect(limit: :infinity, label: "5.1 Answer (6):")
 
     5_809_342
     |> Solution.consecutive_numbers_sum()
-    |> IO.inspect(limit: :infinity, label: "4")
+    |> IO.inspect(limit: :infinity, label: "6 Answer (24):")
+
+    72_316_829
+    |> Solution.consecutive_numbers_sum()
+    |> IO.inspect(limit: :infinity, label: "7 Answer:")
+
+    4
+    |> Solution.consecutive_numbers_sum()
+    |> IO.inspect(limit: :infinity, label: "8 Answer (1):")
   end
 end
 
