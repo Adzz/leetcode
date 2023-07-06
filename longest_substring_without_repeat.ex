e025b6ca0b5c297a87bc6da61c61479d03a6a65d
Adam Lancaster <lancaster28@hotmail.com>
2023-07-06 11:35:48 -0500

    Give up and copy solution

--- longest_substring_without_repeat.ex
+++ longest_substring_without_repeat.ex
@@ -1,57 +1,55 @@
 defmodule Solution do
   @spec length_of_longest_substring(s :: String.t()) :: integer
-  def length_of_longest_substring(""), do: 0
 
   def length_of_longest_substring(s) do
-    consume_string(s, [], 0)
+    s = String.graphemes(s)
+    find_longest(s, s, 0, 0, MapSet.new())
   end
 
-  defp consume_string(<<head::utf8>>, substring, longest) do
-    substring =
-      if head in substring do
-        substring
+  def find_longest([], _, _, longest, _), do: longest
+  def find_longest(_, [], _, longest, _), do: longest
+
+  def find_longest(head, [curr | rest], curr_len, longest, seen) do
+    {head, seen, curr_len} =
+      if MapSet.member?(seen, curr) do
+        contract(head, curr, seen, curr_len)
       else
-        [head | substring]
+        {head, MapSet.put(seen, curr), curr_len + 1}
       end
 
-    max(longest, length(substring))
+    find_longest(head, rest, curr_len, max(longest, curr_len), seen)
   end
 
-  defp consume_string(<<head::utf8, rest::binary>>, [], longest) do
-    consume_string(rest, [head], longest)
+  def contract([el | rest], target, seen, curr_len) when el == target do
+    {rest, seen, curr_len}
   end
 
-  defp consume_string(<<head::utf8, rest::binary>>, [prev | _] = substring, longest) do
-    if head in substring do
-      string =
-        Enum.reduce_while(substring, rest, fn previous, acc ->
-          if previous == head do
-            {:halt, acc}
-          else
-            {:cont, <<previous::utf8, acc::binary>>}
-          end
-        end)
-
-      consume_string(string, [], max(longest, length(substring)))
-    else
-      consume_string(rest, [head | substring], longest)
-    end
+  def contract([el | rest], target, seen, curr_len) do
+    contract(rest, target, MapSet.delete(seen, el), curr_len - 1)
   end
 end
 
 defmodule Main do
   def run() do
-    Solution.length_of_longest_substring("abcabcbb")
-    |> IO.inspect(limit: :infinity, label: "")
+    "abcabcbb"
+    |> Solution.length_of_longest_substring()
+    |> IO.inspect(limit: :infinity, label: "(3)")
 
-    Solution.length_of_longest_substring("bbbbb")
-    |> IO.inspect(limit: :infinity, label: "")
+    "bbbbb"
+    |> Solution.length_of_longest_substring()
+    |> IO.inspect(limit: :infinity, label: "(1)")
 
-    Solution.length_of_longest_substring("pwwkew")
-    |> IO.inspect(limit: :infinity, label: "")
+    "pwwkew"
+    |> Solution.length_of_longest_substring()
+    |> IO.inspect(limit: :infinity, label: "(3)")
 
-    Solution.length_of_longest_substring("dvdf")
+    "dvdf"
+    |> Solution.length_of_longest_substring()
     |> IO.inspect(limit: :infinity, label: "4: (3)")
+
+    "aabaab!bb"
+    |> Solution.length_of_longest_substring()
+    |> IO.inspect(limit: :infinity, label: "5: (3)")
   end
 end
 
